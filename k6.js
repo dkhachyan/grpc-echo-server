545834f60d2f65171cb7f9f8066cefd476438e7f
Denis Khachyan <khachyanda@gmail.com>
2023-09-14 10:37:30 +0300

    fix resources

--- k6.js
+++ k6.js
@@ -1,13 +1,15 @@
 import grpc from 'k6/net/grpc';
+import { check, sleep } from 'k6';
 
 export const options = {
     vus: 100,
     duration: '1m',
     rps: 8000,
+    summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(95)', 'p(99)']
   };
 
 const client = new grpc.Client();
-client.load(['/proto'], 'simple.proto');
+client.load(['proto'], 'simple.proto');
 
 export default () => {
     client.connect('127.0.0.1:8080', {
@@ -16,6 +18,9 @@ export default () => {
   
     const data = { message: 'Bert' };
     const response = client.invoke('Echo/Do', data);
+    check(response, {
+      'status is OK': (r) => r && r.status === grpc.StatusOK,
+    });
 
     console.log(JSON.stringify(response.message));
     client.close();
